a96c1a35ad96d50d82a66dfa9c47d8cb3d8a442b
ZeeyahOke <f.oke@alustudent.com>
2026-06-09 13:18:25 +0200

    feat: feature addded to boss health

--- Assets/Scripts/Boss Scripts/BossHealth.cs
+++ Assets/Scripts/Boss Scripts/BossHealth.cs
@@ -1,74 +1,61 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossHealth : MonoBehaviour {
 
-	private Animator anim;
-	private int health = 1;
+	public int maxHealth = 5;        // Raise this to make the boss tougher.
+	public float hitCooldown = 1f;   // Seconds before the boss can be hit again.
+	public Image healthFill;         // The bar's fill image (Image Type = Filled).
+	public float winDelay = 1.5f;    // Let the death animation play before the win panel.
 
+	private int currentHealth;
+	private Animator anim;
 	private bool canDamage;
 
 	void Awake () {
 		anim = GetComponent<Animator> ();
 		canDamage = true;
+		currentHealth = maxHealth;
+		UpdateHealthBar();
+	}
+
+	void OnTriggerEnter2D(Collider2D target) {
+		if (!canDamage) return;
+		if (target.tag != MyTags.BULLET_TAG) return;
+
+		currentHealth--;
+		canDamage = false;
+		UpdateHealthBar();
+
+		if (currentHealth <= 0) {
+			Defeated();
+		} else {
+			StartCoroutine (WaitForDamage ());
+		}
 	}
 
 	IEnumerator WaitForDamage() {
-		yield return new WaitForSeconds (2f);
+		yield return new WaitForSeconds (hitCooldown);
 		canDamage = true;
 	}
-	
-	void OnTriggerEnter2D(Collider2D target) {
-		if (canDamage) {
-			if (target.tag == MyTags.BULLET_TAG) {
-				health--;
-				canDamage = false;
 
-				if (health == 0) {
-					//
-					GetComponent<BossScript>().DeactivateBossScript();
-					anim.Play("BossDead");
-				}
+	void UpdateHealthBar() {
+		if (healthFill != null) {
+			healthFill.fillAmount = (float)currentHealth / maxHealth;
+		}
+	}
 
-				StartCoroutine (WaitForDamage ());
-			}
+	void Defeated() {
+		GetComponent<BossScript>().DeactivateBossScript(); // Stop the stone attacks.
+		anim.Play("BossDead");
+		Invoke(nameof(WinNow), winDelay);                  // Win shortly after, so the death plays.
+	}
+
+	void WinNow() {
+		if (GameManager.instance != null) {
+			GameManager.instance.WinGame();
 		}
 	}
 
 } // class
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
